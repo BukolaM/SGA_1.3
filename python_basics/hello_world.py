d17da5822d3b64c2c5f0840da363c32aa95d4cf0
Bukola Ajayi <bukolam.ajayi@gmail.com>
2022-07-11 15:41:38 +0100

    my test project

---
+++ python_basics/hello_world.py
@@ -0,0 +1,88 @@
+# # v= 'hello world'
+# # print(v)
+
+# a=6
+# b=5
+# print(a%b)
+
+# c=4
+# # c+=a
+# # print(c)
+
+# c-=a
+# print(c)
+# print(a==b)
+
+
+# animals= ('goat','cat','fish','lion')
+# if 'bear' in animals:
+#     print('seen')
+# elif 'lion' in animals:
+#     print('great')
+# else:
+#     print('not seen')
+
+# my_name= 'shade'
+# if my_name !='bukola':
+#     print('yes')
+# elif my_name == 'shade':
+#     print('found')
+# else:
+#     print('no')
+
+
+# grocery= ['toothpaste','biscuit','jam','brush']
+# grocery.append('cake')
+# print(grocery)
+
+
+# # foodstufs=['yam','egg','rice']
+
+# grocery= ['toothpaste','biscuit','jam','brush']
+# for jess in grocery:
+#     if jess=='jam':
+#         break
+#     print('the item is'+jess)
+
+# grocery.append(foodstufs)
+
+# print(grocery)
+
+# print(type(grocery))
+# grocery.insert(0,'paste')
+# print(grocery)
+
+# grocery.remove('jam')
+# print(grocery)
+
+# count= 2
+# while count<9:
+#     print (count)
+#     count= count + 2
+# # print('done')
+
+
+# count=20
+# while count<30:
+#     print(count)
+#     count= count + 5
+# print('done')
+
+
+
+grocery= ['toothpaste','biscuit','jam','brush']
+if 'book' in grocery:
+    print('yes')
+elif 'make' in grocery:
+    print('noted')
+else:
+    print('love')
+
+
+
+
+
+
+
+
+
