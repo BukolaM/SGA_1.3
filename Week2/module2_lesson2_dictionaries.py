42584b43d73e146a58776cba4e486506e21d77b3
Bukola Ajayi <bukolam.ajayi@gmail.com>
2022-07-17 00:40:44 +0100

    creating a dictionary by calling a function

---
+++ Week2/module2_lesson2_dictionaries.py
@@ -0,0 +1,17 @@
+## To create a dictionary function which takes the square of its input as the output
+## in the range of 6
+def the_square_of_number_range(x):
+    result={}
+    index=0
+
+    for num in x:
+       key = num
+       value = num * num
+       index = index + 1
+       result[key] = value
+    return result
+
+y= the_square_of_number_range(range(6))
+print (y)
+
+
