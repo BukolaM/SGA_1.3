c29037fa8cd68eed8e553be31573fdef876cbaa4
Bukola Ajayi <bukolam.ajayi@gmail.com>
2022-07-19 16:37:46 +0100

    assignment done

--- Week2/manipulate.py
+++ Week2/manipulate.py
@@ -1,14 +1,38 @@
 ##open text files
-with open('cool_docs.txt','r') as cool_docs:
-    read_file=cool_docs.read()
-print(read_file)
+# with open('cool_docs.txt') as cool_docs:
+#     for line in cool_docs.readlines():
+#         print(line)
+
+# with open('cool_docs.txt','w') as cool_docs:
+#     cool_docs.write('this is my story')
+#     print(cool_docs)
 
 
-# with open('cool_docs.txt','r') as cool_docs:
-#     read_file=cool_docs.readlines()
-# for list in read_file:
-#     print(list)
 
-with open('cool_docs.txt','w') as cool_docs:
-    write_file=cool_docs.write('i am okay')
-print(write_file)
+
+
+
+
+
+
+
+# with open ('cool_docs.txt','a') as cool_docs:
+#     cool_docs.write('i have food')
+#     print(cool_docs)
+
+
+# with open ('cool_docs.txt') as cool_docs:
+#     filename= cool_docs.read()
+#     print(filename)
+
+with open ('cool_docs.txt') as cool_docs:
+    for line in cool_docs.readlines(3):
+        print (cool_docs)
+
+
+        with open ('cool_docs.txt') as cool_docs:
+
+
+
+
+        
