b09ed115e07c051740b7f27ed205145d04899ed8
Bukola Ajayi <bukolam.ajayi@gmail.com>
2022-07-25 04:26:59 +0100

    ass on attribute and methods of class

---
+++ python_basics/Week3/module3_lesson2.py
@@ -0,0 +1,39 @@
+
+##• Create an class called car
+# • Give 5 attributes of the car class
+# • Give 5 methods of the car class
+
+class Car:
+    colour = 'black'
+    wheels = 4
+    year = 2014
+    brand = 'Ford'
+    seats = '6 - seater'
+
+    def car_colour(self, colour):
+        self.colour = colour
+
+    def car_wheels(self, wheels):
+        self.wheels = wheels
+
+    def car_year(self, year):
+        self.year = year
+
+    def car_brand(self, brand):
+        self.brand = brand
+
+    def car_seats(self, seats):
+        self.car_seats = seats
+
+
+
+
+
+
+
+
+
+
+
+   
+
