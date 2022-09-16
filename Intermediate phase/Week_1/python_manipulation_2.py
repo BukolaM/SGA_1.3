6d6385709c1988f292f9544b0f4980688961ba88
Bukola Ajayi <bukolam.ajayi@gmail.com>
2022-09-16 17:12:52 +0100

    python manipulation/Users/bukola/Desktop/SGA_1.3/venv/bin/python

--- Intermediate phase/Week_1/python_manipulation_2.py
+++ Intermediate phase/Week_1/python_manipulation_2.py
@@ -1,19 +1,18 @@
 #1. '''Create a function, that repeats the first 2 letters of a word which will be followed by 3 dots and a space after. The string should be ended with a question mark'''. 
 def word_repetition(item):
-        word = item[ :2] + '...' + ' ' + item[ :2] + '...' + ' ' + item
+        word = item[ :2] + '...' + ' ' + item[ :2] + '...' + ' ' + item + '?'
         return word
 
 
 result = word_repetition('victor')
 print(result)
 
-
 result = word_repetition('amazing')
 print(result)
 
-
 result = word_repetition('good')
 print(result)
+
 
 
 #2. Perform a for loop that searches through a string and prints only distinct vowel letters.
@@ -28,19 +27,19 @@ def vowels_search(item):
 result = vowels_search('victor')
 print(result)
 
-
 result = vowels_search('amazing”')
 print(result)
 
- 
 result = vowels_search('good')
 print(result)
+
 
 
 #3. Perform a while loop that requests for a name, if that name is entered, it will be printed else, if user types “end” (this command should be case insensitive), the while loop is terminated
 item = True
 
 while item == True:
+    print('What is your name:' )
     name = input()
 
     if name.lower() == 'end':
